9541e5fc6b797fbbdf110f4afff03c6a3ce894d5
Linus Heinsvig <heinsvig@icloud.com>
2023-05-23 13:10:00 +0000

    Add Restaurant, table and reservation to models.py

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Table(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    table_number = models.IntegerField()
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return f"Table {self.table_number} at {self.restaurant.name}"
+
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE, related_name='reservations')
+    party = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reservations')
+    date = models.DateField()
+    time = models.TimeField()
+    guest_count = models.IntegerField()
+
+    def __str__(self):
+        return f"Reservation {self.id} - {self.user.username} at {self.table.restaurant.name} on {self.date}"
