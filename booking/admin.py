e99b3f2d470ec255f1461f843a5d16a039bdca02
Linus Heinsvig <heinsvig@icloud.com>
2023-05-23 13:23:37 +0000

    Add superuser

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Restaurant, Table, Reservation
+
+admin.site.register(Restaurant)
+admin.site.register(Table)
+admin.site.register(Reservation)
 
 # Register your models here.
